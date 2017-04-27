a297737fd75a98195b093024451ce983464b546d
waynesola <waynesola@users.noreply.github.com>
2017-04-27 21:26:37 +0800

    Ver 1.0 完成：爬取半月谈评论

--- byt_pl/pipelines.py
+++ byt_pl/pipelines.py
@@ -4,8 +4,20 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from items import BytPlItem
+import sqlite3
 
 
 class BytPlPipeline(object):
     def process_item(self, item, spider):
+        if item.__class__ == BytPlItem:  # 此句非必要，在多个items时可能需要用到
+            # 数据库名即目录存放地址
+            conn = sqlite3.connect('C:/Program Files/DB Browser for SQLite/database/database.db')
+            cur = conn.cursor()
+            # 表名是 byt_pl
+            sql = "insert into byt_pl(title,publish,link,text) values (?,?,?,?)"
+            cur.execute(sql, (item['title'], item['publish'], item['link'], item['text'],))
+            conn.commit()
+            cur.close()
+            conn.close()
         return item
